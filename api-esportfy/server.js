8d57a9e776737623fd971b5e6ed8cb1a6c327ebd
Antonio Kelr <antoniofilhodaconceicaorozafil@gmail.com>
2025-02-15 23:04:13 -0300

    alteraçao em rotas

--- api-esportfy/server.js
+++ api-esportfy/server.js
@@ -12,19 +12,19 @@ const PORT = 3001
 
 app.use(cors())
 
-app.get('api/', (req,res)=> {
+app.get('/api/', (req,res)=> {
     res.send('fala oTariosssss')
 } )
-app.get('api/artist', async(req,res)=> {
+app.get('/api/artist', async(req,res)=> {
     res.send( await db.collection("artists").find({}).toArray())
 } )
-app.get('api/songs', async(req,res)=> {
+app.get('/api/songs', async(req,res)=> {
     res.send( await db.collection("songs").find({}).toArray())
 } )
 
 app.use(express.static(path.join(__dirname, '../sportfy/dist')));
 
-app.get('api/songs', async(req,res)=> {
+app.get('*', async(req,res)=> {
     res.sendFile((path.join(__dirname, '../sportfy/dist/index.html')))
 } )
 app.listen(PORT ,()=> { 
--- sportfy/src/services/apiEsportfy.js
+++ sportfy/src/services/apiEsportfy.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import 'dotenv/config'
-const {NODE_ENV} = process.env;
 
-const URL = NODE_ENV === 'development' ?  "http://localhost:3001/api" : '/api';
+const URL = "http://localhost:3001/api";
 
 const responseArtists = await axios.get(`${URL}/artist`);
 const responseSongs = await axios.get(`${URL}/songs`);
